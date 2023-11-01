59f380bc22f8f9004d006e6e2d42b3c8114104ce
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-11-01 16:07:39 +0100

    I have made test scenarios to test the different methods.

--- src/PrimaryClass.java
+++ src/PrimaryClass.java
@@ -3,8 +3,12 @@ public class PrimaryClass {
         System.out.println("This program creates cars and stores the information about them");
 
         Car car1 = new Car("C-class","Mercedes",2021,241_305.16,"Blue",20);
-        car1.delivery(2);
+        System.out.println(car1.toString()+"\n");
         car1.sell(5);
+        System.out.println(car1.getModel()+" "+car1.getBrand()+" "+car1.getYear()+" "+car1.getPrice()+" "+car1.getColor()+" "+car1.getQuantity());
+        car1.setColor("Purple");
+        car1.setYear(2023);
         System.out.println(car1.toString());
+
     }
 }
